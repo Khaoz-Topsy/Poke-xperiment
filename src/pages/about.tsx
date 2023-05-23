345f11a8ca259294a9aeb696c85fb54581caa8a2
Kurt Lourens <hi@kurtlourens.com>
2023-05-23 20:51:07 +0200

    🚧 About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+import { PageHeader } from '../components/common/pageHeader';
+import { CommonLayout } from '../components/common/layout';
+
+export const AboutPage: Component = () => {
+    return (
+        <CommonLayout>
+            <PageHeader text="About"></PageHeader>
+        </CommonLayout>
+    );
+};
+
+export default AboutPage;
